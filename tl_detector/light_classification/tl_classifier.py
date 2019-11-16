ca89a48bf184337d2629725bb3425655c1fa31d7
ashutoshpatel2210 <48851856+ashutoshpatel2210@users.noreply.github.com>
2019-11-16 21:02:56 +0530

    Add files via upload

---
+++ tl_detector/light_classification/tl_classifier.py
@@ -0,0 +1,15 @@
+import os
+
+import cv2
+import numpy as np
+import rospy
+import tensorflow as tf
+from styx_msgs.msg import TrafficLight
+
+
+class TLClassifier(object):
+    def __init__(self):
+        #TODO load classifier
+        pass
+    def get_classification(self):
+        pass
